034af6cf270cca2fe26bde1193ae938c43b30d20
maryam141 <maryam.elsegeny12@gmail.com>
2021-05-28 19:12:26 +0200

    ......

---
+++ app/src/main/java/com/example/geeknews/fragments/InterstedFieldFragment.java
@@ -0,0 +1,31 @@
+package com.example.geeknews.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.geeknews.R;
+
+public class InterstedFieldFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intersted_field, container, false);
+    }
+}
